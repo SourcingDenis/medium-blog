becbba5b19db3a07b334494407ab3bcdd1f54a93
Denys <dinkevich.hr@gmail.com>
2019-06-18 07:26:20 +0300

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Denis Dinkevich',
   githubUrl: 'https://github.com/SourcingDenis/medium-blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://app.netlify.com/sites/focused-hawking-5b2f03/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'SourcingDenis',
